877eda5582dc74ad7dc6d274de5598ac07491b85
Will Fong <willfong@pm.me>
2020-06-09 13:46:13 +0800

    expected cidr_block to contain a network Value with between 16 and 28 significant bits

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_vpc" "EXAMPLE" {
-  cidr_block           = "10.0.0.0/8"
+  cidr_block           = "10.1.0.0/16"
   enable_dns_hostnames = "true"
   tags = {
     Name = "EXAMPLE"
